21641c54666eb4da6b23990f0337967819e10321
malierjia <mariaalejandramoreno@iMac-de-familia.local>
2014-11-26 23:59:33 -0500

    final
    final

--- SQL/tallerlugares.sql
+++ SQL/tallerlugares.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 25-11-2014 a las 07:52:42
+-- Tiempo de generación: 27-11-2014 a las 05:59:07
 -- Versión del servidor: 5.6.20
 -- Versión de PHP: 5.5.15
 
@@ -40,10 +40,10 @@ CREATE TABLE IF NOT EXISTS `lugares` (
 --
 
 INSERT INTO `lugares` (`id`, `tipo`, `nombre`, `lat`, `long`, `direccion`) VALUES
-(1, 'bar', 'El faro', 3.3978839, -76.5396275, 'Calle 66 # 11-18'),
-(2, 'comida', 'Il forno', 3.4215497, -76.542397, 'Roosevelt # 39-2 a 39-100'),
+(1, 'bar', 'El faro', 3.3978839, -76.5396275, 'Calle 66 #11-18'),
+(2, 'comida', 'Il forno', 3.4215497, -76.542397, 'Roosevelt #39-2 a 39-100'),
 (3, 'cine', 'Cinepolis', 3.394453, -76.544488, 'Autosur # 68-1 a 68-99'),
-(4, 'teatro', 'Los cristales', 3.4446208, -76.5466366, 'Carrera 15 # 7O-1 a 7O-99');
+(4, 'teatro', 'Los cristales', 3.4446208, -76.5466366, 'Carrera 15 #7O-1 a 7O-99');
 
 --
 -- Índices para tablas volcadas
